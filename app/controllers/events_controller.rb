95643f6c771a4d7c29aaf1321f406b85ea9721a6
Sergio Diosdado <sgdc_98@hotmail.com>
2019-11-25 01:59:42 -0600

    Visual fix for maps and modal

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,11 @@
 class EventsController < ApplicationController
     before_action :find_event, except: [:index, :create, :new, :my_events, :my_asists, :event_location_suggestions, :event_get_location_details]
     before_action :authenticate_user!
+
+    # Function: index
+    # Gets all the events that are not created by the current user.
+    # Returns:
+    #   Array of Event objects
     def index
         @events = []
         Event.all.each do |event|
@@ -10,17 +15,35 @@ class EventsController < ApplicationController
         end
     end
 
+    # Function: show
+    # Gets a single Event object and sends it to the view.
+    # Also, returns a function to be used at the view.
+    # Returns:
+    #   Single Event object
+    #   Function attending?
     def show
         @attending = attending?
     end
 
+    # Function: new
+    # Gets an empty single event object and sends it to the view.
+    # Returns:
+    #   Empty single Event object
     def new
         @event = Event.new
     end
 
+    # Function: edit
+    # Gets a single event object with all the attributes and sends it to the view.
+    # Returns:
+    #   Single Event object
     def edit
     end
 
+    # Function: my_assists
+    # Gets an array of events which the current user is attending to and sends it to the view.
+    # Returns:
+    #   Array of Event objects
     def my_asists
         @events = []
         Attend.all.each do |attend|
@@ -30,6 +53,10 @@ class EventsController < ApplicationController
         end
     end
 
+    # Function: my_events
+    # Gets an array of events created by the current user and sends it to the view.
+    # Returns:
+    #   Array of Event objects
     def my_events
         @events = []
         Event.all.each do |event|
@@ -39,37 +66,54 @@ class EventsController < ApplicationController
         end
     end
 
+    # Function: create
+    # Creates a new instance of Event, pass all the parameters from the user and make a persistent record in the database.
     def create
         @event = Event.create(creator: current_user.id,title: params[:event][:title], cover: params[:event][:cover], date: params[:event][:date], description: params[:event][:description])
         redirect_to @event
     end
 
+    # Function: update
+    # Updates an existing instance of Event, pass all the parameters from the user and update the persistent record in the database.
     def update
         @event.update(title: params[:event][:title], cover: params[:event][:cover], date: params[:event][:date], latitude: params[:event][:latitude], longitude: params[:event][:longitude], description: params[:event][:description])
-        
         redirect_to @event
     end
 
+    # Function: create_attend
+    # Creates a new instance of Attend, pass all the parameters from the user and make a persistent record in the database.
     def create_attend
         @attend = Attend.create(userID: current_user.id, eventID: @event.id)
         redirect_to @event
     end
 
+    # Function: confirmation
+    # Loads a partial view of a confirmation menu in another view.
     def confirmation
-      flash[:danger]= "Are you sure you want to delete this event? <a class='button is-danger is-block center-block' style='width: 50%;'  data-method='delete' href='/events/#{@event.id}'> Yes</a>"
+      flash[:danger]= "<p class='m-b-md'>Are you sure you want to delete this event?</p> <a class='button is-danger is-block center-block' style='width: 50%;'  data-method='delete' href='/events/#{@event.id}'> Yes</a>"
       redirect_to my_events_path
     end
 
+    # Function: destroy
+    # Deletes an existing instance of Event from the database.
     def destroy
         @event.destroy
         redirect_to my_events_path
     end
     
+    # Function: event_location_suggestions
+    # Gets the best five matches for the current address typed and returns them.
+    # Returns:
+    #   Array of Suggestions object
     def event_location_suggestions
         @here = Apis::HereApi.new(params[:address]).get_suggestions
         render json: @here
     end
 
+    # Function: event_get_location_details
+    # Gets the details of an specific location.
+    # Returns
+    #   LocationDetails object
     def event_get_location_details
         @loc = Apis::HereApi.new("").get_location_details(params[:locationid])
         render json: @loc
@@ -78,10 +122,18 @@ class EventsController < ApplicationController
 
     private
 
+    # Function: find_event
+    # Gets the data of the current event
+    # Returns:
+    #   Event object
     def find_event
         @event = Event.find(params[:id])
     end
 
+    # Function: attending?
+    # Returns true or false depending of the existance of an specific Attend object
+    # Returns:
+    #   Boolean scalar
     def attending?
         user_id = current_user.id
         event_id = @event.id
--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,43 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "events#index"
 
+  # Index of the application (Home)
   get "events", to: "events#index"
+
+  # Forms to create a new event
   get "events/new", to: "events#new", as: :event_new
+
+  # View of the events created by the current user
   get "events/my_events", to: "events#my_events", as: :my_events
+
+  # View of the events which the current user is attending
   get "events/my_asists", to: "events#my_asists", as: :my_asists
+
+  # Popup menu to confirm the deletion of event
   get "events/:id/confirmation", to: "events#confirmation", as: :event_confirmation
+
+  # Calls the API for the location details
   get "events/event_get_location_details/:locationid", to: 'events#event_get_location_details', as: :event_get_location_details
+
+  # Calls the API for the best five suggestions
   get "events/event_location_suggestions/:address", to: 'events#event_location_suggestions', as: :event_location_suggestions
+  
+  # View to show details of an specific event
   get "events/:id", to: "events#show"
+
+  # Forms to edit details of an specific event
   get "events/:id/edit", to: "events#edit", as: :event_edit
 
+  # Action to delete aan specific event
   delete "events/:id", to: "events#destroy"
 
+  # Acction to update an specific event
   patch "events/:id", to: "events#update", as: :event
+
+  # Action to create an specific event
   post "events", to: "events#create"
+
+  # Action to create a new Attend model record
   post "events/:id", to: "events#create_attend", as: :create_attend
 
 end
